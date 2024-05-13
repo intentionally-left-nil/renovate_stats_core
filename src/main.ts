b873a162814ee1788d941ded14ec28462186184e
Anil Kulkarni <akulkarni@anaconda.com>
2024-05-13 13:52:30 -0700

    Try to grab all the PR's

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,35 @@
-import * as core from '@actions/core'
-import { wait } from './wait'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { wait } from './wait';
+import { Octokit } from '@octokit/rest';
+import type { Endpoints } from '@octokit/types';
+type PullRequests =
+  Endpoints['GET /repos/{owner}/{repo}/pulls']['response']['data'];
+
+async function getPRs(): Promise<PullRequests> {
+  let createdAfter: Date | undefined;
+  const createdAfterInput: string = core.getInput('createdAfter') ?? '';
+  if (createdAfterInput) {
+    createdAfter = new Date(createdAfterInput);
+  }
+
+  const token: string = process.env.GITHUB_TOKEN ?? '';
+  const octokit = new Octokit({ auth: token });
+  const { owner, repo } = github.context.repo;
+  let pulls = await octokit.paginate(octokit.pulls.list, {
+    owner,
+    repo,
+    base: 'main'
+  });
+  pulls = pulls.filter(pull =>
+    pull.labels.some(label => label.name === 'renovate')
+  );
+
+  if (createdAfter) {
+    pulls = pulls.filter(pull => new Date(pull.created_at) > createdAfter);
+  }
+  return pulls;
+}
 
 /**
  * The main function for the action.
@@ -7,21 +37,24 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const ms: string = core.getInput('milliseconds');
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-    core.debug('Hello, world!')
+    core.debug(`Waiting ${ms} milliseconds ...`);
+    core.debug('Hello, world!');
 
     // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(new Date().toTimeString());
+    await wait(parseInt(ms, 10));
+    core.debug(new Date().toTimeString());
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', new Date().toTimeString());
+
+    const prs = await getPRs();
+    core.setOutput('all', JSON.stringify(prs));
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
